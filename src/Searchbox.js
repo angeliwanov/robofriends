390a0fb5aed8fe389c6dbd5a32e33d1a1519eaef
angeliwanov <angel.iwanov@gmail.com>
2022-09-26 16:27:06 +0300

    Searchbox

---
+++ src/Searchbox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Searchbox = () => {
+    return (
+        <div className="pa2">
+            <input type='search' placeholder="search robots" className="pa3 ba b--green bg-lightest-blue"/>
+        </div>
+    )
+}
+
+export default Searchbox;
