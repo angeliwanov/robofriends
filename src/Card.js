7906889938dc889fd909f5cda9fba4800c84acf8
angeliwanov <angel.iwanov@gmail.com>
2022-09-26 10:51:10 +0300

    Card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+
+
+const Card =  ({id, name, email}) => {
+    return(
+        <div className="tc bg-light-green dib pa3 br3 ma2 grow bw2 shadow-5">
+            <img alt='robots' src={`https://robohash.org/${id}?200x200`}/>
+            <div>
+                <h1>{name}</h1>
+                <p>{email}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
