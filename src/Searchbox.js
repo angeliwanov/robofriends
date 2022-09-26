a962eb161d0bbf8481ffcda56c4726c6722a5e11
angeliwanov <angel.iwanov@gmail.com>
2022-09-26 17:01:30 +0300

    constructor + onSearchChange

--- src/Searchbox.js
+++ src/Searchbox.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const Searchbox = () => {
+const Searchbox = ({SearchChange}) => {
     return (
         <div className="pa2">
-            <input type='search' placeholder="search robots" className="pa3 ba b--green bg-lightest-blue"/>
+            <input 
+            type='search' 
+            placeholder="search robots" 
+            className="pa3 ba b--green bg-lightest-blue"
+            onChange={SearchChange}
+            />
         </div>
     )
 }
